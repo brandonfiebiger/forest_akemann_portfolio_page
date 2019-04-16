d65fc3aff741b54d0f653fd7d2c5950b2515b3a5
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-04-16 16:14:09 -0600

    Implement splash page animation with setTimeouts.

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 
 
 $(document).ready(() => {
+  
+  setTimeout(() => {
+    $('.splash-page_logo').addClass('slider');
+  }, 1000);
+
+  setTimeout(() => {
+    $('.splash-page_logo').addClass('invisible');
+  }, 2000);
+
+  setTimeout(() => {
+    $('.splash-page').addClass('display_none');
+    $('.projects-nav').removeClass('display_none');
+  }, 3000)
+
+
   const tags = ['lunaire', 'zogra', 'newmoon', 'hawaii'];
-  const times = [1000, 2000, 3000, 4000]
+  const times = [4000, 5000, 6000, 7000]
 
   for (let i = 0; i < tags.length; i++) {
 
