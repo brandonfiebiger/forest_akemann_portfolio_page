880537896b98311e11f0ae3a87a1de5ec504a765
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-05-06 00:24:34 -0600

    Delete unused code from slide up animation.

--- js/slide_up.js
+++ js/slide_up.js
@@ -7,7 +7,7 @@ class SlideUp extends Highway.Transition {
   // responsible for page coming in
   in({ from, to, done }) {
 
-    const timeline = new TimelineLite();
+    const timeline = new TimelineLite(); 
 
     timeline.fromTo(to, 0.2, {top: '100%'}, {top: '0%', onComplete: function(){
       from.remove();
@@ -17,7 +17,6 @@ class SlideUp extends Highway.Transition {
     .fromTo(to.children[1].children[1], .2, {opacity: '0'}, {opacity: '1'}, '+=.75')
     .fromTo(to.children[2], .2, {opacity: '0', transform: 'translateY(6rem)'}, {opacity: 1, transform: 'translateY(5rem)'}, '+=1')
     .fromTo(to.children[3], .5, { display: 'none' }, { display: 'block'}, '+=1')
-    // .FromTo('.lunaire-page_header', 5, {opacity: '0'}, {opacity: '1'}, 5)
   }
   //responsible for page going out
   out({ from, done }) {
